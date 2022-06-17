6dbb5f1642e7bdb98a9b40aa7509ce554201d38f
Collins Sirwani <sircollo71@gmail.com>
2022-06-18 00:43:52 +0300

    This will add static file settings, css and js static files

--- neighbourhood/urls.py
+++ neighbourhood/urls.py
@@ -1,7 +1,12 @@
 from django.urls import re_path as url
+from django.conf import settings
+from django.conf.urls.static import static
 from . import views
+
 urlpatterns = [
     url('$',views.index, name='index'),
     url('signup/', views.signup, name='register'),
     url('signin/', views.signin, name='signin'),
 ]
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
--- neighbourhoodapp/settings.py
+++ neighbourhoodapp/settings.py
@@ -126,6 +126,9 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 # STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 cloudinary.config( 
