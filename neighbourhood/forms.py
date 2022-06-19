1360ac04918374c05bb44f2ac7353086a28f3991
Collins Sirwani <sircollo71@gmail.com>
2022-06-19 23:13:13 +0300

    Feat: add first and last name, phone fields and widget attributes to signup and login forms

--- neighbourhood/forms.py
+++ neighbourhood/forms.py
@@ -4,14 +4,17 @@ from django.contrib.auth.forms import *
 from django.contrib.auth.models import User
 
 class SignUpForm(UserCreationForm):
-    username = forms.CharField(label='username', max_length=50, widget=forms.TextInput(attrs={'placeholder':'Username'}), required=True)
-    email = forms.EmailField(label='email', widget=forms.TextInput(attrs={'placeholder':'Email'}), required=True)
-    password1 = forms.CharField(label='password', max_length=50, widget=forms.PasswordInput(attrs={'placeholder':'Password'}), required=True)
-    password2 = forms.CharField(label='password', max_length=50, widget=forms.PasswordInput(attrs={'placeholder':'Confirm Password'}), required=True)
+    first_name = forms.CharField(label='firstname', max_length=50, widget=forms.TextInput(attrs={'class':'form-control','placeholder':'First Name'}), required=True)
+    last_name = forms.CharField(label='lastname', max_length=50, widget=forms.TextInput(attrs={'class':'form-control','placeholder':'Last Name'}), required=True)
+    username = forms.CharField(label='username', max_length=50, widget=forms.TextInput(attrs={'class':'form-control','placeholder':'User Name'}), required=True)
+    phone_number = forms.IntegerField(label='phonenumber', widget=forms.TextInput(attrs={'class':'form-control','placeholder':'Phone Number'}), required=True)
+    email = forms.EmailField(label='email', widget=forms.TextInput(attrs={'class':'form-control','placeholder':'Email'}), required=True)
+    password1 = forms.CharField(label='password', max_length=50, widget=forms.PasswordInput(attrs={'class':'form-control','placeholder':'Password'}), required=True)
+    password2 = forms.CharField(label='password', max_length=50, widget=forms.PasswordInput(attrs={'class':'form-control','placeholder':'Confirm Password'}), required=True)
     
 class SignInForm(AuthenticationForm): 
-  username = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Username'})) 
-  password = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder': 'Password'}))
+  username = forms.CharField(label='username', max_length=50, widget=forms.TextInput(attrs={'class':'form-control','placeholder':'User Name'}), required=True)
+  password = forms.CharField(label='password', max_length=50, widget=forms.PasswordInput(attrs={'class':'form-control','placeholder':'Password'}), required=True)
   class Meta:
     model = User
     fields = ['username','password']
