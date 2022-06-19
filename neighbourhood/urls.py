1d5ff45b878549f041becccfaaa6cd8286ddde95
Collins Sirwani <sircollo71@gmail.com>
2022-06-19 23:34:39 +0300

    Feat: add logout user path

--- neighbourhood/urls.py
+++ neighbourhood/urls.py
@@ -2,11 +2,13 @@ from django.urls import re_path as url
 from django.conf import settings
 from django.conf.urls.static import static
 from . import views
+from .views import *
 
 urlpatterns = [
     url('$',views.index, name='index'),
     url('signup/', views.signup, name='register'),
     url('signin/', views.signin, name='signin'),
+    url('logout/', SignOutView.as_view(), name='logout'),
 ]
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
